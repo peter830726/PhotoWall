2355eb1a29aea624487d02be599e28aa8af04fc4
student <student@studentde-iMac-221.local>
2017-05-05 16:20:57 +0800

    init with travis

---
+++ PhotoWall2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoWall2
+//
+//  Created by student on 2017/5/5.
+//  Copyright © 2017年 CSIE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PhotoWall2Tests/PhotoWall2Tests.m
@@ -0,0 +1,39 @@
+//
+//  PhotoWall2Tests.m
+//  PhotoWall2Tests
+//
+//  Created by student on 2017/5/5.
+//  Copyright © 2017年 CSIE. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PhotoWall2Tests : XCTestCase
+
+@end
+
+@implementation PhotoWall2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
