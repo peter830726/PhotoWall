9ddf23726e3b250fbfda23e3a933c0b3de20370a
student <student@studentde-iMac-221.local>
2017-05-12 17:18:04 +0800

    view

---
+++ PhotoWall2/View/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  PhotoWall2
+//
+//  Created by student on 2017/5/12.
+//  Copyright © 2017年 CSIE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
